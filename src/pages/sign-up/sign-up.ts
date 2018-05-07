0be937e8d08b8dbfc5612bd91eebf7dc5271c5ca
Yannik Inniger <yannik.inniger@siroop.ch>
2018-05-07 16:56:42 +0200

    Add link to signup page

--- src/pages/sign-up/sign-up.ts
+++ src/pages/sign-up/sign-up.ts
@@ -1,13 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
-/**
- * Generated class for the SignUpPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
-
 @IonicPage()
 @Component({
   selector: 'page-sign-up',
@@ -15,8 +8,10 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class SignUpPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams) {}
 
+  singUpWithEmail() {
+    this.navCtrl.push('SignUpEmail');
   }
 
 }
