42e0b2236561b7625e419bb87a73bfeca8ac3c1f
Yannik Inniger <yannik.inniger@siroop.ch>
2018-06-08 13:45:53 +0200

    Add tournament detail view

---
+++ src/pages/tournament-detail/tournament-detail.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {Tournament} from "../../model/tournament";
+
+/**
+ * Generated class for the TournamentDetailPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-tournament-detail',
+  templateUrl: 'tournament-detail.html',
+})
+export class TournamentDetailPage {
+
+  tournament: Tournament;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.tournament = navParams.get('tournament');
+  }
+
+
+}
