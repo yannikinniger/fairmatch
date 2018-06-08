92a99b917a3eab7b651de82376b4343ba4b3e739
Yannik Inniger <yannik.inniger@siroop.ch>
2018-06-08 14:03:10 +0200

    Reload of tournaments, display tournament details

--- src/pages/tournament-overview/tournament-overview.ts
+++ src/pages/tournament-overview/tournament-overview.ts
@@ -14,12 +14,20 @@ export class TournamentOverviewPage {
 
   constructor(
     public navCtrl: NavController,
-    tournamentProvider: TournamentProvider,
-  ) {
-    tournamentProvider.getTournaments()
+    private tournamentProvider: TournamentProvider,
+  ) {}
+
+  ionViewDidEnter() {
+    this.tournamentProvider.getTournaments()
       .then(tournaments => {
         this.tournaments = tournaments;
       });
+  }
+
+  showTournamentDetails(id: string) {
+    const tournament = this.tournaments.find(tournament => tournament.id == id);
+    this.navCtrl.push('TournamentDetailPage', {tournament: tournament});
+    console.log(tournament);
   }
 
   showTournamentFirstPage() {
